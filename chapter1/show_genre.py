f8ccd4d7caf37718038e4a9fb16d0d635f2fa33f
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-02-16 08:07:33 +0330

    edit in messages that show to user for choose favorite genre

--- chapter1/show_genre.py
+++ chapter1/show_genre.py
@@ -1,15 +1,18 @@
 # get numbers of user and return number:int
 def get_person():
-    number_of_person = int(input())
+    number_of_person = int(input('enter numbers of persons? '))
     return number_of_person
 
 
 def person_information_genre(numbers: int):  # input number of people that we get from get_person()
     # define all genre in dictionary
     all_genres = {'Horror': 0, 'Romance': 0, 'Comedy': 0, 'History': 0, 'Adventure': 0, 'Action': 0}
+    print('please enter your name and choose 3 of favorite genres?( choose from these'
+          ' option: Horror, Romance, Comedy, History, Adventure, Action)')
     for i in range(numbers):  # loop in range of numbers
-        genres = list(map(str, input('').split(' ')))  # get name and 3 genres
-        if len(genres) == 4:
+        genres = list(map(str, input().split(' ')))
+        # get name and 3 genres
+        if len(genres) == 4:  # check len genres list that contains name and 3 genres.
             for genre in genres[1:]:
                 if genre == 'Horror':
                     all_genres['Horror'] += 1
@@ -24,12 +27,12 @@ def person_information_genre(numbers: int):  # input number of people that we ge
                 elif genre == 'Action':
                     all_genres['Action'] += 1
                 else:
-                    print('enter genre according all genre list')
+                    print('enter genre according genre list')
                     break
     all_genres = dict(sorted(all_genres.items(), key=lambda item: item[0]))  # sorted by key according alphabet
-    all_genres = dict(sorted(all_genres.items(), key=lambda item: item[1], reverse=True))  # sorted by value by decreasing
+    all_genres = dict(sorted(all_genres.items(), key=lambda item: item[1], reverse=True))  # sorted by value
     for key, value in all_genres.items():  # output is vertically
-        print(f"{key}: {value}")
+        print(f"{key} : {value}")
 
 
 number_of_persons = get_person()
