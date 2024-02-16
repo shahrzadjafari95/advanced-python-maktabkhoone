680a7ab8cc3d66d39c6819645e8eed75d4f17266
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-02-16 19:50:59 +0330

    define get_olympic_members() for get information such as gender, name and language programming and then separate with '.' , then capitalize name and sort according gender and name.

--- chapter1/computer_olympic.py
+++ chapter1/computer_olympic.py
@@ -1,4 +1,24 @@
 def get_person():
-    number_of_persons = int(input())
+    number_of_persons = int(input('enter number of member:'))
     return number_of_persons
+
+
+def get_olympic_members(number: int):
+    list_of_member = []
+    print('enter your gender, namae and language programming')
+    for i in range(number):
+        # separate user input according '.' and put to the list
+        information_member = list(map(str, input().split('.')))
+        list_of_member.append(information_member)  # each information_list input append to list_of_members
+    for member in list_of_member:
+        member[1] = member[1].capitalize()  # capitalize second item in each nested list
+    list_of_member.sort(key=lambda x: (x[0], x[1]))  # sorted according first and second item in list
+    return list_of_member
+
+
+numbers = get_person()
+all_member = get_olympic_members(numbers)
+# print(" ".join(str(item) for item in user))
+for user in all_member:
+    print(f'{user[0]} {user[1]} {user[2]}')
 
