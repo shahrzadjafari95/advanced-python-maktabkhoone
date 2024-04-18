f210c253b67d4456f8f476cb097def623731bec2
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-04-18 22:23:10 +0330

    define limit= 2000 for extract 2000 first ads from bama.ir, after that with use Beautidulsoup parse html page and then extract all items from bama.ir with use find_all and related tags from inspect of this site

--- final_project.py
+++ final_project.py
@@ -8,4 +8,9 @@ url = 'https://bama.ir/car'
 driver = webdriver.Chrome()
 driver.get(url)
 
+html_page = driver.page_source
+
+limit = 2000
+soup = BeautifulSoup(html_page, 'html.parser')
+items = soup.find_all('div', attrs={'class': 'bama-ad-holder'})[:limit]
 
