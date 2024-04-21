dd914fa504c247d4d6a2527d3add984458180fd8
shahrzadjafari95 <135174912+shahrzadjafari95@users.noreply.github.com>
2024-04-21 15:34:35 +0330

    get 10 pages from truecar.com with define an url, and use loop for read these informations about this site, after that, these informations put into the all_list, and then we save all of them into the all_cars.csv for read and use into the machine learning method.

--- final_project.py
+++ final_project.py
@@ -10,42 +10,41 @@ mydb = mysql.connector.connect(
     password="11071399",
     database="truecar_all_cars"
 )
-
-url = 'https://www.truecar.com/used-cars-for-sale/listings/?buyOnline=true'
-
-driver = webdriver.Chrome()
-driver.get(url)
-
-html_page = driver.page_source
-
-limit = 20
-soup = BeautifulSoup(html_page, 'html.parser')
-items = soup.find_all('div', attrs={'class': 'flex w-full flex-col'})[:limit]
-
 cursor = mydb.cursor()
 
-all_cars = []
-for item in items:
-    car = []
-    brand = item.find('div', attrs={'class': 'w-full truncate font-bold'}).text[5:]
-    car.append(brand)
-    price = item.find('h3', attrs='heading-4 normal-case flex items-center').text
-    car.append(price)
-    performance = item.find('div', attrs={'class': 'flex items-center truncate text-xs'}).text
-    car.append(performance)
-    all_cars.append(car)
-    sql = "INSERT INTO car_informations (brand ,price, performance) VALUES (%s, %s, %s)"
-    val = (brand, price, performance)
-    cursor.execute(sql, val)
-    mydb.commit()
-    # print(car)
+url = 'https://www.truecar.com/used-cars-for-sale/listings/?buyOnline=true&page='
+for page in range(1, 10):
+    url = 'https://www.truecar.com/used-cars-for-sale/listings/?buyOnline=true&page={}'.format(page)
+
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    html_page = driver.page_source
+
+    # limit = 5
+    soup = BeautifulSoup(html_page, 'html.parser')
+    items = soup.find_all('div', attrs={'class': 'flex w-full flex-col'})
+
+    all_cars = []
+    for item in items:
+        car = []
+        brand = item.find('div', attrs={'class': 'w-full truncate font-bold'}).text[5:]
+        car.append(brand)
+        performance = item.find('div', attrs={'class': 'flex items-center truncate text-xs'}).text[:-3]
+        int_performance = int(performance.replace("k", ""))
+        car.append(int_performance)
+        price = item.find('h3', attrs='heading-4 normal-case flex items-center').text[1:]
+        int_price = int(price.replace(",", ""))
+        car.append(int_price)
+        all_cars.append(car)
+        sql = "INSERT INTO car_informations (brand, performance, price ) VALUES (%s, %s, %s)"
+        val = (brand, price, performance)
+        cursor.execute(sql, val)
+        mydb.commit()
+
+    with open('all_cars.csv', 'a', newline='', ) as file:
+        writer = csv.writer(file)
+        for row in all_cars:
+            writer.writerow(row)
 mydb.close()
 
-# print(all_cars)
-with open('all_cars.csv', 'a', newline='', ) as file:
-    writer = csv.writer(file)
-    for row in all_cars:
-        writer.writerow(row)
-
-
-# with open('all_cars.csv','r', newline=''):
